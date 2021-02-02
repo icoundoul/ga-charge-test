aa1156ed94ddd692ee8e99c3865c4cbb731ed93d
Ibrahima COUNDOUL <i.coundoul-ext@lefebvre-sarrut.eu>
2021-02-02 19:23:21 +0100

    corrections

--- src/test/scala/ga/Defaults.scala
+++ src/test/scala/ga/Defaults.scala
@@ -2,7 +2,7 @@ package ga
 
 object Defaults {
 
-  val DEFAULT_BASE_URL: String = "http://ga-int-alb-2103090581.eu-west-1.elb.amazonaws.com"
+  val DEFAULT_BASE_URL: String = "https://qlf-ga-ws.gestion-des-acces.fr/gaServices"
   val DEFAULT_V_USERS: Int = 10
 
 }
--- src/test/scala/ga/scenarios/HttpProtocol.scala
+++ src/test/scala/ga/scenarios/HttpProtocol.scala
@@ -6,14 +6,18 @@ import io.gatling.core.Predef._
 import io.gatling.http.Predef._
 import ga.Constants.{BASE_URL, PROXY_HOST, PROXY_PORT, V_USERS}
 
+
 object HttpProtocol {
-  
-  val INSTANCE = http
-    .baseURL("http://webpage")
+
+  var INSTANCE = http
+    .baseUrl(BASE_URL)
+    .inferHtmlResources(BlackList(""".*.ico""", """.*.js""", """.*.css"""), WhiteList())
+    //.acceptHeader("application/json, text/plain, */*")
     .acceptHeader("text/html,text/xml,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8")
     .acceptEncodingHeader("gzip, deflate")
-    .acceptLanguageHeader("en-US,en;q=0.5")
-    .connection("keep-alive")
+    .acceptLanguageHeader("fr-FR,fr;q=0.9,en-US;q=0.8,en;q=0.7")
+    .userAgentHeader("Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.132 Safari/537.36")
+    .headers(Map("Proxy-Connection" -> "keep-alive"))
 
 
   if (PROXY_HOST != null && PROXY_PORT != null) {
